3f8ad7f09d30ce2a740f534feea05e16781c8880
TazakiN <tazkia.nizami@gmail.com>
2024-04-04 23:18:30 +0700

    feat: implement bfs

--- src/logic/bfs.go
+++ src/logic/bfs.go
@@ -1,24 +1,35 @@
 package logic
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 func BFS(linkMulai string, linkTujuan string) []string {
-	result := Result{
-		Method:     "BFS",
-		LinkAwal:   linkMulai,
-		LinkTujuan: linkTujuan,
+	queue := []*Node{}
+	start := newNode(linkMulai)
+	hasil := []string{}
+
+	titleTujuan := getPageTitle(linkTujuan)
+
+	queue = append(queue, start)
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if getPageTitle(current.link) == titleTujuan {
+			for current != nil {
+				hasil = append(hasil, getPageTitle(current.link))
+				current = current.parent
+			}
+			break
+		}
+
+		neighbours := getAllATag(current.link)
+		for _, neighbourLink := range neighbours {
+			neighbour := newNode(neighbourLink)
+			if !neighbour.visited {
+				neighbour.visited = true
+				neighbour.distance = current.distance + 1
+				neighbour.parent = current
+				queue = append(queue, neighbour)
+			}
+		}
 	}
-
-	// proses pencarian jalur di sini
-
-	jsonResult, err := json.Marshal(result)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return nil
-	}
-
-	return []string{string(jsonResult)}
+	return hasil
 }
